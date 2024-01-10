3661a1a1954c451f0250c89bb2be103522709df0
cym1212 <william5680@naver.com>
2024-01-10 22:51:54 +0900

    swagger 작업 1

--- src/main/java/io/mohajistudio/tangerine/prototype/infra/upload/controller/S3UploadController.java
+++ src/main/java/io/mohajistudio/tangerine/prototype/infra/upload/controller/S3UploadController.java
@@ -3,6 +3,7 @@ package io.mohajistudio.tangerine.prototype.infra.upload.controller;
 
 import io.mohajistudio.tangerine.prototype.domain.post.dto.PlaceBlockImageDTO;
 import io.mohajistudio.tangerine.prototype.infra.upload.service.S3UploadService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class S3UploadController {
     private final S3UploadService s3UploadService;
 
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "AWS S3에 이미지 업로드", description = "이미지 값을 넘기면 S3에 이미지를 저장하고 URL과 확장자를 리턴합니다.")
     public ResponseEntity<PlaceBlockImageDTO> uploadImage(@NotNull @RequestPart("imageFile") MultipartFile multipartFile) {
 
         PlaceBlockImageDTO placeBlockImageDTO = s3UploadService.uploadImageToS3(multipartFile);
