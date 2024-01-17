3f26e2a8c0b3ca46e80964bcb1ff0275e309f393
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2024-01-17 15:23:29 +0900

    [TREN-1] 운영 환경에서 에러 발생시 디스코드 웹훅 메시지 발송

--- src/main/java/io/mohajistudio/tangerine/prototype/global/error/GlobalExceptionHandler.java
+++ src/main/java/io/mohajistudio/tangerine/prototype/global/error/GlobalExceptionHandler.java
@@ -3,7 +3,14 @@ package io.mohajistudio.tangerine.prototype.global.error;
 import io.mohajistudio.tangerine.prototype.global.enums.ErrorCode;
 import io.mohajistudio.tangerine.prototype.global.error.exception.BusinessException;
 import io.mohajistudio.tangerine.prototype.global.error.exception.UrlNotFoundException;
+import io.mohajistudio.tangerine.prototype.infra.webhook.dto.DiscordWebhookDTO;
+import io.mohajistudio.tangerine.prototype.infra.webhook.dto.EmbedObjectDTO;
+import io.mohajistudio.tangerine.prototype.infra.webhook.dto.FieldDTO;
+import io.mohajistudio.tangerine.prototype.infra.webhook.service.WebhookService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +24,14 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.net.BindException;
+import java.util.List;
 
 @Slf4j
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    private final WebhookService webhookService;
+    private final Environment environment;
 
     /**
      * javax.validation.Valid or @Validated 으로 binding error 발생시 발생한다.
@@ -28,8 +39,9 @@ public class GlobalExceptionHandler {
      * 주로 @RequestBody, @RequestPart 어노테이션에서 발생
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest httpServletRequest) {
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.INVALID_INPUT_VALUE);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
@@ -38,8 +50,9 @@ public class GlobalExceptionHandler {
      * ref https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-ann-modelattrib-method-args
      */
     @ExceptionHandler(BindException.class)
-    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e, HttpServletRequest httpServletRequest) {
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.INVALID_INPUT_VALUE);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
@@ -48,7 +61,7 @@ public class GlobalExceptionHandler {
      * 주로 @RequestParam enum으로 binding 못했을 경우 발생
      */
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e, HttpServletRequest httpServletRequest) {
         final ErrorResponse response = ErrorResponse.of(e);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
@@ -57,58 +70,66 @@ public class GlobalExceptionHandler {
      * 지원하지 않은 HTTP method 호출 할 경우 발생
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest httpServletRequest) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.METHOD_NOT_ALLOWED);
         return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest httpServletRequest) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.HTTP_MESSAGE_NOT_READABLE);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.HTTP_MESSAGE_NOT_READABLE);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    protected ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+    protected ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(MissingServletRequestParameterException e, HttpServletRequest httpServletRequest) {
         log.error("handleMissingServletRequestParameterException", e);
-
         final ErrorResponse response = ErrorResponse.of(ErrorCode.MISSING_PARAMETER, e.getParameterName());
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.MISSING_PARAMETER);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
-        log.error("handleEntityNotFoundException", e);
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e, HttpServletRequest httpServletRequest) {
+        log.error("BusinessException", e);
         final ErrorCode errorCode = e.getErrorCode();
         final ErrorResponse response = ErrorResponse.of(errorCode);
+        sendWebhook(httpServletRequest, e.getMessage(), errorCode);
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+    protected ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest httpServletRequest) {
         log.error("handleEntityNotFoundException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(NoResourceFoundException.class)
-    protected ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e) {
+    protected ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e, HttpServletRequest httpServletRequest) {
         final ErrorResponse response = ErrorResponse.of(ErrorCode.URL_NOT_FOUND);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.URL_NOT_FOUND);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    protected ResponseEntity<ErrorResponse> handleInvalidDataAccessApiUsageException(DataIntegrityViolationException e) {
+    protected ResponseEntity<ErrorResponse> handleInvalidDataAccessApiUsageException(DataIntegrityViolationException e, HttpServletRequest httpServletRequest) {
         log.error("handleInvalidDataAccessApiUsageException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.DATA_INTEGRITY_VIOLATION);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.DATA_INTEGRITY_VIOLATION);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
-    
+
     @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<ErrorResponse> handleInvalidDataAccessApiUsageException(IllegalArgumentException e) {
+    protected ResponseEntity<ErrorResponse> handleInvalidDataAccessApiUsageException(IllegalArgumentException e, HttpServletRequest httpServletRequest) {
         log.error("handleIllegalArgumentException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.ILLEGAL_ARGUMENT);
+        sendWebhook(httpServletRequest, e.getMessage(), ErrorCode.ILLEGAL_ARGUMENT);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
@@ -116,5 +137,20 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleUrlNotFoundException(UrlNotFoundException e) {
         final ErrorResponse response = ErrorResponse.of(ErrorCode.URL_NOT_FOUND);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    public void sendWebhook(HttpServletRequest httpServletRequest, String errorMessage, ErrorCode errorCode) {
+        if ("prod".equals(environment.getActiveProfiles()[0])) {
+        FieldDTO methodField = FieldDTO.createInlineField("Method", httpServletRequest.getMethod());
+        FieldDTO endpointField = FieldDTO.createInlineField("Endpoint", httpServletRequest.getRequestURI());
+        FieldDTO clientIpField = FieldDTO.createInlineField("ClientIp", httpServletRequest.getRemoteAddr());
+
+        FieldDTO errorCodeField = FieldDTO.createInlineField("ErrorCode", errorCode.getCode());
+        FieldDTO statusCodeField = FieldDTO.createInlineField("StatusCode", String.valueOf(errorCode.getStatus()));
+        FieldDTO errorMessageField = FieldDTO.createField("ErrorMessage", errorCode.getMessage());
+        EmbedObjectDTO embedObjectDTO = EmbedObjectDTO.createErrorEmbedObject("ErrorMessage", errorMessage, List.of(methodField, endpointField, clientIpField, errorCodeField, statusCodeField, errorMessageField));
+        DiscordWebhookDTO discordWebhookDTO = DiscordWebhookDTO.createErrorMessage("Spring Boot Server Error", List.of(embedObjectDTO));
+        webhookService.sendMessage(discordWebhookDTO);
+        }
     }
 }
