a587314f4b5dcea914a1db97f0793c01792d82cd
cym1212 <william5680@naver.com>
2024-02-17 16:11:32 +0900

    [TREN 45_1] 게시글 상세조회 1차구현

--- src/main/java/io/mohajistudio/tangerine/prototype/admin/post/repository/AdminPostRepository.java
+++ src/main/java/io/mohajistudio/tangerine/prototype/admin/post/repository/AdminPostRepository.java
@@ -5,18 +5,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface AdminPostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-
-
     @Query("SELECT p FROM Post p WHERE p.title LIKE %:searchKeyword% OR p.member.memberProfile.nickname LIKE %:searchKeyword% OR p.member.memberProfile.name LIKE %:searchKeyword%")
     Page<Post> findByTitleOrNicknameContaining(String searchKeyword, Pageable pageable);
 
+
+    Optional<Post> findAdminDetailsById(@Param("id") Long id);
 
 //    @Modifying(clearAutomatically = true)
 //    @Query("update Post p set p.deletedAt = NULL where p.id = :id AND p.deletedAt IS NOT NULL")
--- src/main/java/io/mohajistudio/tangerine/prototype/admin/post/service/AdminPostService.java
+++ src/main/java/io/mohajistudio/tangerine/prototype/admin/post/service/AdminPostService.java
@@ -2,11 +2,14 @@ package io.mohajistudio.tangerine.prototype.admin.post.service;
 
 import io.mohajistudio.tangerine.prototype.admin.post.repository.AdminPostRepository;
 import io.mohajistudio.tangerine.prototype.domain.post.domain.Post;
+import io.mohajistudio.tangerine.prototype.global.error.exception.UrlNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,11 @@ public class AdminPostService {
 
     public Page<Post> adminSearch(String searchKeyword, Pageable pageable) {
         return adminPostRepository.findByTitleOrNicknameContaining(searchKeyword, pageable);
+    }
+
+    public Post findAdminPostDetails(Long id) {
+        Optional<Post> findPost = adminPostRepository.findAdminDetailsById(id);
+        if (findPost.isEmpty()) throw new UrlNotFoundException();
+        return findPost.get();
     }
 }
